103960a1dd0b6aacb05c58b9563de3c49b20b91d
Anh Ngo <brennanngo1292000@gmail.com>
2021-01-15 03:03:38 +0700

    update

--- src/screen/app/Debug.js
+++ src/screen/app/Debug.js
@@ -3,13 +3,14 @@ import { useContext } from 'react';
 import Layout from '../../component/App/Core/Layout';
 import Text from '../../component/Text';
 import StoreApp from '../../Context';
+import { colors } from '../../theme';
 
 
 function Menu() {
     const {debugData} = useContext(StoreApp)
     return (
         <Layout scroll>
-            <Text>
+            <Text color={colors.text1}>
                 {debugData}
             </Text>
         </Layout>
